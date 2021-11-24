13cc50d33abfa78e8d5ca2c7cec57175b6006458
Lucille Delisle <lucille.delisle@epfl.ch>
2021-11-24 13:19:11 +0100

    change way to get datasetID at upload

--- external_scripts/upload_and_add_log.sh
+++ external_scripts/upload_and_add_log.sh
@@ -9,7 +9,10 @@ dataset_name=$7
 omero_dir_or_file=$8
 temp_file=$9
 ${omero_path} import -s ${omero_server} -u ${omero_user} -w ${omero_password} --depth ${depth} -T Project:name:"${project_name}"/Dataset:name:"${dataset_name}" ${omero_dir_or_file} &> ${temp_file} 
-dId=$(${omero_path} search -s ${omero_server} -u ${omero_user} -w ${omero_password} --field "name" --ids-only --style plain "Dataset" "${dataset_name}" | awk -F ":" 'END{print $NF}')
+# Using omero search may give strange results (see https://github.com/lldelisle/omeroDubouleUploadAnnotate/issues/5)
+# So the log is parsed to get the Dataset ID:
+dId=$(cat "${temp_file}" | grep "Import target specifies container: Dataset" | awk -F ":" 'NR==1{print $NF}')
+# Warning: omero upload gives a deprecation warning...
 oFile=$(${omero_path} upload -s ${omero_server} -u ${omero_user} -w ${omero_password} $temp_file | awk '{print $NF}')
 fAnn=$(${omero_path} obj -s ${omero_server} -u ${omero_user} -w ${omero_password} new FileAnnotation file=${oFile})
 ${omero_path} obj -s ${omero_server} -u ${omero_user} -w ${omero_password} new DatasetAnnotationLink parent=Dataset:${dId} child=${fAnn}
