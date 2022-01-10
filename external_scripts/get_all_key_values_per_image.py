34465d5f0a3dcf6328ea86c3f80f7967bcb60043
Lucille Delisle <lucille.delisle@epfl.ch>
2022-01-10 14:11:01 +0100

    update get_all_key_values_per_image to get also images without key values + dataset_id and subset to specific dataset or project

--- external_scripts/get_all_key_values_per_image.py
+++ external_scripts/get_all_key_values_per_image.py
@@ -6,24 +6,38 @@ import sys
 import os
 import pandas as pd
 
-def get_images_df(username, password, host, port, user_only):
+def get_images_df(username, password, host, port, user_only, selected_project, selected_dataset):
     with BlitzGateway(username, password, host=host, port=port, secure=True) as conn:
         # inspired by https://gist.github.com/will-moore/12d31c026bfe5adaea4b2341494069a0#file-metadata_queries-py
         params = omero.sys.ParametersI()
-        query = """select 
-    image.id, image.name, dataset.name, project.name, mv.name, mv.value, project.details.owner.id
+        # First get the structure (image - dataset - project)
+        query1 = """select 
+    image.id, image.name, dataset.name, dataset.id, project.name, project.details.owner.id
 from 
     Image image
 join image.datasetLinks d
 join d.parent dataset
 join dataset.projectLinks p
 join p.parent project
+"""
+        # Then get the key values
+        query2 = """select 
+    image.id, mv.name, mv.value
+from 
+    Image image
 join image.annotationLinks ial
 join ial.child a
 join a.mapValue mv
 """
         if user_only:
-            query += f"where project.details.owner.id = {conn.getEventContext().userId}\n"
+            query1 += f"where project.details.owner.id = {conn.getEventContext().userId}"
+            query2 += f"""
+join image.datasetLinks d
+join d.parent dataset
+join dataset.projectLinks p
+join p.parent project
+where project.details.owner.id = {conn.getEventContext().userId}
+"""
         else:
             # We want to get the correspondance id name:
             group = conn.getGroupFromContext()
@@ -34,16 +48,39 @@ join a.mapValue mv
                 ids_to_name[o.getId()] = o.getOmeName()
             for m in members:
                 ids_to_name[m.getId()] = m.getOmeName()
-        result = conn.getQueryService().projection(query, params)
+        if selected_dataset is not None:
+            query1 += f"where dataset.id = {selected_dataset}"
+            query2 += f"""
+join image.datasetLinks d
+join d.parent dataset
+where dataset.id = {selected_dataset}
+"""
+        if selected_project is not None:
+            query1 += f"where project.id = {selected_project}"
+            query2 += f"""
+join image.datasetLinks d
+join d.parent dataset
+join dataset.projectLinks p
+join p.parent project
+where project.id = {selected_project}
+"""
+        result1 = conn.getQueryService().projection(query1, params)
+        result2 = conn.getQueryService().projection(query2, params)
     result_dict = {}
-    for row in result:
-        image_id, image_name, dataset_name, project_name, mv_name, mv_value, owner_id = [x.val for x in row]
+    for row in result1:
+        image_id, image_name, dataset_name, dataset_id, project_name, owner_id = [x.val for x in row]
         if image_id not in result_dict:
             result_dict[image_id] = {'id': image_id, 'image.name': image_name,
-                                    'dataset.name':dataset_name, 'project.name': project_name}
+                                    'dataset.name':dataset_name, 'dataset.id':dataset_id,
+                                    'project.name': project_name}
             if not user_only:
                 result_dict[image_id]['user.omename'] = ids_to_name[owner_id]
-        result_dict[image_id][mv_name] = mv_value
+    for row in result2:
+        image_id, mv_name, mv_value = [x.val for x in row]
+        # We only add key values for images
+        # Which are within datasets/projects:
+        if image_id in result_dict:
+            result_dict[image_id][mv_name] = mv_value
     return(pd.DataFrame([result_dict[id] for id in result_dict]))
 
 
@@ -56,6 +93,10 @@ argp.add_argument("-w", "--password", help="OMERO password or file with password
 argp.add_argument("--onlyUserProjects",
                   help = "Restrict search to projects associated to the user.",
                   action="store_true")
+argp.add_argument("--selectedProject", type=int, default=None,
+                  help="ID of project to restrict the search to.")
+argp.add_argument("--selectedDataset", type=int, default=None,
+                  help="ID of dataset to restrict the search to.")
 argp.add_argument("-o", "--output", default=sys.stdout,
                   type=argparse.FileType('w'),
                   help="Output csv file.")
@@ -67,5 +108,6 @@ if os.path.exists(args.password):
 else:
     password = args.password
 df = get_images_df(args.user, password, args.server, args.port,
-                   args.onlyUserProjects)
+                   args.onlyUserProjects, args.selectedProject,
+                   args.selectedDataset)
 df.to_csv(args.output, index=False)
